ec827bd7b611af871422a63a5b8092cb0185e4f5
SebastienLatouche <sebastien.latouche.28@gmail.com>
2020-12-20 12:23:18 +0100

    insert components in app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,12 +3,22 @@ import React from 'react';
 
 // == Import
 // import reactLogo from './react-logo.svg';
+import Header from 'src/components/Header';
+import Nav from 'src/components/Nav';
+import Page from 'src/components/Page';
+import Footer from 'src/components/Footer';
+
 import './app.scss';
 
 // == Composant
 const App = () => (
   <div className="app">
-    <h1>Composant : App</h1>
+    <Header />
+    <div className="app_flex">
+      <Nav />
+      <Page />
+    </div>
+    <Footer />
   </div>
 );
 
