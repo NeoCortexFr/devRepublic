9e1d8066fdf2842e7bac49de1b15f9e69d700a74
SebastienLatouche <sebastien.latouche.28@gmail.com>
2020-12-21 14:20:56 +0100

    test srcoll by components

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
 import Nav from 'src/components/Nav';
+import Para from 'src/components/Para';
 
 import './page.scss';
 
 const Page = () => (
   <div className="page">
     <Nav />
+    <Para /><br />
+    <Para /><br />
+    <Para /><br />
+    <Para /><br />
+    <Para /><br />
+    <Para /><br />
+    <Para /><br />
+    <Para /><br />
+    <Para /><br />
+    <Para /><br />
   </div>
 );
 
