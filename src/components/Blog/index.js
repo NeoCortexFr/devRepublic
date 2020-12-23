f805321eb0cbd0eb5581e404d6f64071562665c5
NeoCortexFr <sebastien.latouche.28@gmail.comgit config --global user.name NeoCortexFrgit config --global user.email sebastien.latouche.28@gmail.com>
2020-12-23 12:55:21 +0100

    design of blog section done

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -4,7 +4,20 @@ import './blog.scss';
 
 const Blog = () => (
   <div className="blog">
-    <h2>Blog</h2>
+    <h2 className="blog_h2">Blog</h2>
+    <h3 className="blog_h3">Welcome sur le blog de Dev'Republic</h3>
+    <div className="section">
+      <article className="blog_content">
+        <p>
+          Vous pouvez retrouver les articles via la navigation de gauche.
+        </p>
+      </article>
+      <nav className="blog_nav">
+        <ul>
+          <li>Titre 1</li>
+        </ul>
+      </nav>
+    </div>
   </div>
 );
 export default Blog;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,15 +9,15 @@ const Footer = () => (
         <a href="#">Instagram</a>
       </li>
       <li className="footer_object-title">
-        <a href="#">LinkedIn</a>
+        <a href="https://www.linkedin.com/in/sebastien-latouche-99a4b915b/">LinkedIn</a>
       </li>
       <li className="footer_object-title">
-        <a href="#">GitHub</a>
-      </li>
-      <li className="footer_object-title">
-        <a href="#">Apothéose</a>
+        <a href="https://github.com/NeoCortexFr?tab=repositories">GitHub</a>
       </li>
     </ul>
+    <p className="footer_owner">
+      &copy; 2021 by NeoCortexFr
+    </p>
   </div>
 );
 
