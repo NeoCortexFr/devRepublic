01caa65d620bf4d7a202159728aca7fc7645374d
NeoCortexFr <sebastien.latouche.28@gmail.comgit config --global user.name NeoCortexFrgit config --global user.email sebastien.latouche.28@gmail.com>
2021-01-17 15:20:19 +0100

    counter ok

---
+++ src/components/Count/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './count.scss';
+
+const Count = () => {
+  // eslint-disable-next-line prefer-const
+  let [count, setCount] = useState(0);
+  const [jaime, setJaime] = useState(0);
+  const [jaimePas, setJaimePas] = useState(0);
+  return (
+    <div className="count">
+      <h2 className="count_title">Le compteur de click</h2>
+      <p className="count_content">
+        Vous avez cliquez <span className="count_content-bold">{count}</span> fois.
+      </p>
+      <p className="count_buttons">
+        <button type="button" className="count_button" onClick={() => setCount(count + 1)}>CLiquez ici !</button>
+        {/* eslint-disable-next-line no-return-assign */}
+        <button type="button" className="count_button" onClick={() => setCount(count = 0)}>Réinitialiser...</button>
+      </p>
+      <p className="count_state">
+        Ici, j'utilise le hook "useState" pour définir l'état initial du compteur,
+        que j'initialise à 0.
+        Sur le bouton de comptage, j'utilise une fonction fléchée sur le "onClick",
+        où j'ajoute "+ 1".
+        Sur le bouton "Réinitialiser...", je donne une valeur de 0 à count.
+      </p>
+      <p className="count_state">
+        Vous pouvez aller voir le code dans la partie <Link to="/tutos/compteur">Tutos</Link>
+      </p>
+      <h2 className="count_title">J'aime / J'aime pas</h2>
+      <p className="count_content">
+        <button type="button" className="count_button" onClick={() => setJaime(jaime + 1)}>J'aime {jaime}</button>
+        <button type="button" className="count_button" onClick={() => setJaimePas(jaimePas + 1)}>J'aime Pas {jaimePas}</button>
+      </p>
+    </div>
+  );
+};
+
+export default Count;
--- src/components/Web/index.js
+++ src/components/Web/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
 
+import Count from 'src/components/Count';
+
 const Web = () => (
   <div className="web">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ligula augue,
-    hendrerit in arcu ut, ultricies mattis lacus. Vestibulum vestibulum feugiat viverra.
-    Donec eu neque a mi cursus mattis vitae at lorem. Nullam eu auctor lorem, nec
-    lobortis risus. Cras nec elit commodo risus vestibulum varius. Fusce vitae odio
-    pulvinar felis convallis molestie. Nam sodales lorem id tortor ultricies blandit.
-    Vivamus felis libero, laoreet quis augue in, interdum dictum velit. Pellentesque blandit,
-    ante eu dictum cursus, ligula nisl iaculis augue, ac pretium ligula dolor in diam.
-    Proin id sodales metus. Nullam dapibus blandit risus a imperdiet. Sed mattis massa
-    at dolor pretium eleifend.<br />
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ligula augue,
-    hendrerit in arcu ut, ultricies mattis lacus. Vestibulum vestibulum feugiat viverra.
-    Donec eu neque a mi cursus mattis vitae at lorem. Nullam eu auctor lorem, nec
-    lobortis risus. Cras nec elit commodo risus vestibulum varius. Fusce vitae odio
-    pulvinar felis convallis molestie. Nam sodales lorem id tortor ultricies blandit.
-    Vivamus felis libero, laoreet quis augue in, interdum dictum velit. Pellentesque blandit,
-    ante eu dictum cursus, ligula nisl iaculis augue, ac pretium ligula dolor in diam.
-    Proin id sodales metus. Nullam dapibus blandit risus a imperdiet. Sed mattis massa
-    at dolor pretium eleifend.
+    <Count />
   </div>
 );
 export default Web;
